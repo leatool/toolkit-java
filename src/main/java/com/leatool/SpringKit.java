48df44e3a89417cea89c2921bf83a607ac881dcb
shijinyuan <13152720003@163.com>
2019-02-27 23:45:37 +0800

    Initialize the example project

---
+++ src/main/java/com/leatool/SpringKit.java
@@ -0,0 +1,17 @@
+package com.leatool;
+
+public class SpringKit {
+
+
+    /**
+     *  判断是否为空
+     * @param str
+     * @return
+     */
+    public static boolean isEmpty(String str){
+
+        return null == str || str.length()==0;
+
+    }
+
+}
